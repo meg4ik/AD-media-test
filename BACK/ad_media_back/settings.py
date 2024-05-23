5abdb758611e09ada228f10e16dc5e58e8ec07e9
Pavlo Ryndin <meg4mag@gmail.com>
2024-05-23 11:28:15 +0300

    swagger

--- BACK/ad_media_back/settings.py
+++ BACK/ad_media_back/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'corsheaders',
+    'drf_yasg',
 
     'advertising.apps.AdvertisingConfig',
     'links.apps.LinksConfig',
@@ -125,6 +126,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 
